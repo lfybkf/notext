3ff877135fa10bfd02a352678341028b34e37c15
lfybkf <lfybkfa@gmail.com>
2014-05-26 19:24:00 +0300

    new files

---
+++ Notext/Abzac.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bdb
+{
+	class Abzac
+	{
+		static const string BEGIN = "=======";
+		//====================
+		int line;
+		string content;
+		//=======================
+
+		public bool contains(IList<string> words)
+		{
+			return true;
+		}//function
+	}//class
+}//ns
---
+++ Notext/Paket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bdb
+{
+	class Paket
+	{
+		public static const string wordAny = "*";
+		static const IEnumerable<Abzac> empty = new Abzac[0];
+		//====================
+		string fileName;
+		string wordMain;
+		DateTime dtIndexed;
+		IEnumerable<Abzac> content = new List<Abzac>();
+
+
+		//================
+		public IEnumerable<Abzac> findAll(IList<string> words)
+		{
+			if (words.Count < 2)
+				return empty;
+
+			if (words[0] != wordMain || words[0] != wordAny)
+				return empty;
+
+			return content.Where(a => a.contains(words));
+
+		}//function
+
+	}//class
+}//ns
